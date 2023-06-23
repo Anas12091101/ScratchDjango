46588fc6572ef278f3ac84fc4546889e501a74db
Muhammad Anas <muhammad.anas@A006-00605.local>
2023-06-23 15:21:12 +0500

    Updated the admin listing page for User

--- ScratchDjango/User/admin.py
+++ ScratchDjango/User/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import User
 
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["name", "email", "is_active"]
+    search_fields = ["name", "email", "is_active"]
+
+
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
